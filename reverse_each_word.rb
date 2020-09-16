380a073a4317974fbda613dc55ec2ea4ad079497
Carl J <carlj1881@yahoo.com>
2020-09-16 06:22:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   array.each do |word|
 
-   word.reverse
+   word.reverse!
         binding.pry
     string = array.join(" ")
     return string
