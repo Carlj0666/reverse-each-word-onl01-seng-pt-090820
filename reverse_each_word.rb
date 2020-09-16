77a9486215d1e1e58507e501465bf76a9dd893e5
Carl J <carlj1881@yahoo.com>
2020-09-16 06:39:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,3 +10,12 @@ def reverse_each_word(string)
   new_string.join(" ")
 end
 
+def reverse_each_word(string)
+  new_string = []
+  array = string.split(" ")
+  array.collect do |word|
+  word.reverse!
+  new_string.push(word)
+  end
+  new_string.join(" ")
+end
